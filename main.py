78bc29aa288b0225db3688a94335c68d9b4b4913
h-poincare <adam.d.shetler@gmail.com>
2023-04-04 19:05:23 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 import tkinter as tk
+import sys
 
 class App:
     def __init__(self, master):
@@ -22,18 +24,28 @@ class App:
     def run_script(self, input1, input2):
         # your script code here
         output = f"Input 1: {input1}\nInput 2: {input2}"
+        print("TESTING")
         return output
 
     def get_inputs(self):
         input1 = self.entry1.get()
         input2 = self.entry2.get()
+        
+        # redirect stdout to text widget
+        sys.stdout = TextRedirector(self.text)
+
         output = self.run_script(input1, input2)
-        self.text.delete("1.0", tk.END)
-        self.text.insert(tk.END, output)
 
+    # restore stdout when done
+    sys.stdout = sys.__stdout__
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+class TextRedirector:
+    def __init__(self, widget):
+        self.widget = widget
 
+    def write(self, string):
+        self.widget.insert(tk.END, string)
+
+root = tk.Tk()
+app = App(root)
+root.mainloop()
